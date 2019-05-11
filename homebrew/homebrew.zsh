1919afee9a7b0e76257a0a7311072d13a0df05fa
erdaltsksn <erdal@taskesen.me>
2019-05-11 13:14:14 +0300

    feat: (homebrew) Use consistent naming for aliases
    why: The other aliases use a naming convention like `packageDoAbc`.

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -3,7 +3,7 @@
 ################################################################################
 
 # Remove all brew packages using Brewfile as a whitelist
-alias brewclean="cd ~/.dotfiles/homebrew && brew bundle cleanup --force && cd -"
+alias brewClean="cd ~/.dotfiles/homebrew && brew bundle cleanup --force && cd -"
 
 # Fetch the newest of all formulae, Upgrade outdated, and remove old versions
 alias brewUpdate="brew update && brew upgrade && brew cleanup"
