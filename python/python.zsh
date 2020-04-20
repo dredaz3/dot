e104a80fb761fcf498d0e62f1ff9fa604f8cf2c3
erdaltsksn <erdal@taskesen.me>
2020-04-20 14:16:13 +0300

    feat: (python) Delete `p` alias for `python`

--- python/python.zsh
+++ python/python.zsh
@@ -2,8 +2,5 @@
 ########################## PYTHON ##############################################
 ################################################################################
 
-# Python shortcut
-alias p="/usr/local/opt/python/libexec/bin/python"
-
 # Remove Python's Byte-compiled / optimized / DLL files and folders
 alias pyClean="find . | grep -E '(__pycache__|\.pyc|\.pyo$)' | xargs rm -rf"
