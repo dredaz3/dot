cc14dc2a4dc64cab202b1a62df2d0c10da6338fd
erdaltsksn <erdal@taskesen.me>
2020-05-06 06:11:49 +0300

    feat: (mac) Delete the alias 'macUpdate'
    why: It's unnecessary to have two different command for same function.
    pre: `macUpdate`
    now: `mas upgrade`

--- mac/mac.zsh
+++ mac/mac.zsh
@@ -4,6 +4,3 @@
 
 # macOS folder attributes / meta-data files
 alias dsClean="find . -type f -name '*.DS_Store' -ls -delete"
-
-# Download and install all your mac software
-alias macUpdate="sudo softwareupdate -i -a"
