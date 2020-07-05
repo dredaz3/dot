ce1fe562717143fc22a86d9af300022463d89c60
erdaltsksn <erdal@taskesen.me>
2020-07-05 21:46:22 +0300

    feat: (system) Remove the old dotfilesUpdate alias

--- system/dotfiles.zsh
+++
@@ -1,6 +0,0 @@
-################################################################################
-########################## DOTFILES ############################################
-################################################################################
-
-# Update dotfiles
-alias dotfilesUpdate="curl https://raw.githubusercontent.com/erdaltsksn/dotfiles/master/scripts/update.sh | bash"
